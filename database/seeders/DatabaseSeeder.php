14273663a24ba77189533213d631d9bd221c4b8f
phamngocchien2004@@ <114908347+phamngocchien2004@users.noreply.github.com>
2023-10-06 19:15:44 +0700

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+use Tests\Fixtures\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\Category::factory("10")->create();
+        \App\Models\Car::factory("20")->create();
+
+
     }
 }
